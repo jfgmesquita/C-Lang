259255fe26dcee40403553b7a172211cf9a37a36
Joao Mesquita <jolitas2005@gmail.com>
2024-10-18 12:12:27 +0100

    Update WC

--- Word Count/wc.c
+++ Word Count/wc.c
@@ -1,30 +1,25 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
     char ch;
-    char ultimo;
-    int caracteres, palavras, linhas;
-    caracteres = 0;
-    palavras = 0;
-    linhas = 0;
+    char lastChar = ' ';
+    int characters = 0, words = 0, lines = 0;
 
-    while((ch = getchar()) != EOF) {
-        caracteres += 1;
+    while ((ch = getchar()) != EOF) {
+        characters++;
 
-        if(ch == '\n') {
-            linhas += 1;
+        if (ch == '\n') {
+            lines++;
         }
-        else if((ultimo == ' ' || ultimo == '\n' ) && (ch != ' ' && ch != '\n')) {
-            palavras += 1;
+        if ((lastChar == ' ' || lastChar == '\n' || lastChar == '\t') && (ch != ' ' && ch != '\n' && ch != '\t')) {
+            words++;
         }
-        ultimo = ch;
+        lastChar = ch;
     }
 
-    palavras += 1;
+    printf("Number of lines: %d\n", lines);
+    printf("Number of words: %d\n", words);
+    printf("Number of characters: %d\n", characters);
 
-    printf("Numero de linhas: %d\n", linhas);
-    printf("Numero de palavras: %d\n", palavras);
-    printf("Numero de letras: %d\n", caracteres);
-    
     return 0;
 }
