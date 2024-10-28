0c5d356427e7154a2cb9eaad2b0512e589f1f0ab
Joao Mesquita <jolitas2005@gmail.com>
2024-10-28 16:47:06 +0000

    OPERATOR PRECEDENCE

---
+++ Operator Precedence/op.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#define disp(v) printf("%s = %d\n", #v, v)
+
+int main(void){
+  int a = 0;
+  int b = 0;
+  int c = 15;
+
+  a = b = c += 5;
+
+  disp( a );
+  disp( b );
+  disp( c );
+
+  return 0;
+}
