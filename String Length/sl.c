974357a233594013420b203f08b69f92335b5cb9
João Mesquita <jolitas2005@gmail.com>
2024-11-12 21:55:54 +0000

    White Spaces

--- String Length/sl.c
+++ String Length/sl.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 int main(int argc, char** argv) {
-    if (argc < 2) {
+    if(argc < 2) {
         printf("Usage: %s <string1> <string2> ... <stringN>\n", argv[0]);
         return 1;
     }
 
-    for (int i = 1; i < argc; i++) {
+    for(int i = 1; i < argc; i++) {
         char* s = argv[i];
         int len = strlen(s);
         printf("Length(\"%s\"): %d\n", s, len);
